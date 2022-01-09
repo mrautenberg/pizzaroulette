0abdef1e380210b1d072d7fbf9a056d5a22f1bfa
Max Rautenberg <max.rautenberg89@gmail.com>
2022-01-09 12:44:07 +0100

    basic layout fixes pre-styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,22 +15,57 @@ function App() {
       .catch((err) => console.log(err));
   }, [showPizza]);
 
+  // Show pizza on setShowPizza --> toggle to the view when we see a pizza
+
+  // Random pizza but pizzeria already selected
+  const randomPizza = () => {
+    // useFetch to fetch from API
+
+    console.log("random pizza");
+    if (!showPizza) {
+      console.log("hurru");
+      setShowPizza(true);
+    }
+  };
+
+  // Full random, pizza & pizzeria randomized
+  const fullRandom = () => {
+    // useFetch to fetch from API
+
+    console.log("full random");
+    if (!showPizza) {
+      setShowPizza(true);
+    }
+  };
+
   const buttonContainer = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    display: "grid",
+    gridTemplateColumns: "4fr 1fr",
+    gridTemplateRows: "auto",
+
+    width: "60%",
+    margin: "0 auto",
+    maxWidth: "600px",
   };
 
   return (
-    <div className="App">
+    <div>
       <h1>Pizza Roulette</h1>
 
       {showPizza && <Pizza />}
 
       <div style={buttonContainer}>
-        <button onClick={() => setShowPizza(!showPizza)}>Slumpa</button>
-        <button onClick={() => console.log("full random")}>Full random</button>
+        <button onClick={() => randomPizza()}>Slumpa</button>
+        <button onClick={() => fullRandom()}>Full random</button>
+        {/* select should store the value for selected pizzeria
+            i.e. setPizzeria("lillItalien") if lillaItalien is selected 
+        */}
+        <select name="" id="">
+          <option value="lillaItalien">Lilla Italien</option>
+          <option value="chrillesPizzeria">Chrilles Pizzeria</option>
+          <option value="kaktusenPizzeria">Kaktusen Pizzeria</option>
+          <option value="pizzeriaHornet">Pizzeria Hörnet</option>
+        </select>
         <button onClick={() => console.log("slumpa pizzeria")}>Tärning</button>
       </div>
     </div>
